816995c871bdb575e9f082fd92b970f4ee4fda78
maccalla <bcmk1220158@gn.iwasaki.ac.jp>
2021-05-26 20:53:15 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+function Counter() {
+  // State: a counter value
+  const [counter, setCounter] = useState(0);
+
+  // Action: code that causes an update to the state when something happens
+  const increment = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
+  // View: the UI definition
+  return (
+    <div>
+      Value: {counter} <button onClick={increment}>Increment</button>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Counter />
+    </div>
+  );
+}
